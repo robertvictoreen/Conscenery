5fc78da3229bff1882e7e399a9e8a31b5c100dec
robertvictoreen <robert.victoreen@gmail.com>
2019-01-09 13:57:32 -0800

    Live

---
+++ contact.php
@@ -0,0 +1,48 @@
+ <?php
+
+ require_once("session.php");
+
+ if($loggedout==0){
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  	if (!isset($_POST['user'])){
+        die("User required");
+    }
+	  $data = mysqli_real_escape_string($db,$_POST["user"]);
+
+	   $userinfo = "SELECT * FROM USERS WHERE USERNAME='".$data."';";
+	 if ($user_result = mysqli_query($db, $userinfo)) {
+
+		 $count = mysqli_num_rows($user_result);
+
+		 if ($count < 1) die("User not found");
+
+   		$user_data = mysqli_fetch_array($user_result,MYSQLI_ASSOC);
+
+	   	$userid = mysqli_real_escape_string($db,$user_id);
+
+		if ($user_data["ID"] == $user_id) die("User not found");
+
+	 $sql = "SELECT * FROM NETWORK WHERE (USER1 = '".$userid."' AND USER2 = '".$user_data["ID"]."') OR (USER2 = '".$userid."' AND USER1 = '".$user_data["ID"]."');";
+      $obj = mysqli_query($db,$sql);
+
+  //    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+     // $active = $row['active'];
+
+
+      $count = mysqli_num_rows($obj);
+
+	  if ($count < 1) {
+
+		  $sql = "INSERT INTO NETWORK (USER1, USER2, STATUS) VALUES ('".$userid."', '".$user_data["ID"]."', '1');";
+
+		  if (!mysqli_query($db,$sql)) {
+			  die(mysqli_error($db));
+		  }
+
+	  }}
+	  exit("1");
+  }
+ }
+ ?>
---
+++ pending_contacts.php
@@ -0,0 +1,61 @@
+  <?php
+  require_once("session.php");
+  if ($loggedout==0){
+  $userid = mysqli_real_escape_string($db,$user_id);
+    $query = "SELECT * FROM NETWORK WHERE STATUS<>'0' AND (USER1='".$userid."' OR USER2='".$userid."');";
+
+if ($result = mysqli_query($db, $query)) {
+
+$count = 0;
+    /* fetch associative array */
+    while ($row = mysqli_fetch_assoc($result)) {
+		$count++;
+		if ($row["USER1"] == $user_id)
+		$otheruser = $row["USER2"];
+		else $otheruser = $row["USER1"];
+		
+		?>
+        <div class="user_row"><p>
+        <?php
+		
+		 $userinfo = "SELECT * FROM USERS WHERE ID='".$otheruser."';";
+	 if ($user_result = mysqli_query($db, $userinfo)) {
+		 
+   		$user_data = mysqli_fetch_array($user_result,MYSQLI_ASSOC);
+		echo $user_data["USERNAME"]; 
+		
+		if ($row["STATUS"] == '1') {
+		//purgatory
+			if ($row["USER1"]!=$userid){
+			?>
+            <a class="contact_request" href="connect.php?id=<?php echo $user_data["ID"] ?>">Accept request</a>
+            <?php
+			} else {
+				echo " (Requested)";
+			}
+			?>
+            <a class="contact_request" href="connect.php?id=<?php echo $user_data["ID"] ?>&status=1"> Cancel</a>
+            <?php
+		} else if ($row["STATUS"] == '2') {
+			//blocked, check direction
+			if (($row["USER1"]==$userid && $row["DIRECTION"]=="0") || ($row["USER2"]==$userid && $row["DIRECTION"]=="1")){
+				?>
+				 <a class="contact_request" href="connect.php?id=<?php echo $user_data["ID"] ?>&status=1">Unblock</a>
+				 <?php
+				
+			}
+		}
+	
+	 }
+	 
+		?> </div></p> <?php
+	}
+	if ($count ==0) {
+		?>
+        <div class="user_row"><p>No pending contacts</p></div>
+		<?php
+	}
+}
+  }
+  
+  ?>
